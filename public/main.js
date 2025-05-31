a04396e19ce9f0bcb1603fd8919efb0b4d973584
tgsegata <tgsegata@gmail.com>
2025-05-30 22:23:24 -0400

    Updating code on js

--- public/main.js
+++ public/main.js
@@ -1,42 +1,68 @@
-console.log("hellooooooooo world");
+// API website "marine.json"
+const apiUrl = 'https://api.weatherapi.com/v1/marine.json?q="New York"&key=59320ff7bbb247e0b88222430252905';
 
-// API
-const apiUrl = 'https://api.weatherapi.com/v1/current.json?q="New York"&key=59320ff7bbb247e0b88222430252905';
 
 // Function
-async function getCurrentWeather () {
+async function WeatheringToday () {
     const apiRes = await fetch (apiUrl)
     const result = await apiRes.json();
 
-    // Whole API data
-    // console.log ('API response: ', JSON.stringify(result));
+    // LETS CREATE A SPACE SO USER WRITE THE CITY / $ INSIDE THE WEBSITE URL
 
-    // Weather API
-    const currentTempCelsius = result.current.temp_c;
-    console.log ('Current Weather: ', currentTempCelsius);
-
-    // City Name
+    // City
     const cityName = result.location.name;
-    console.log ('City Name: ', cityName);
+    console.log ('City: ', cityName);
+
+    // Region
+    const regionName = result.location.name;
+    console.log ('Region: ', regionName);
+
+    // Date
+    const dateToday = result.forecast.forecastday[0].date;
+    console.log('Date:', dateToday);
+
+    // Icon weather
+    const iconWeather = result.forecast.forecastday[0].day.condition.icon;
+    console.log('Weather Icon:', iconWeather);
+
+    // Text weather
+    const textWeather = result.forecast.forecastday[0].day.condition.text;
+    console.log('Weather:', textWeather);
+
+    // Sunrise
+    const sunrise = result.forecast.forecastday[0].astro.sunrise;
+    console.log('Sunrise:', sunrise);
+
+    // Sunset
+    const sunset = result.forecast.forecastday[0].astro.sunset;
+    console.log('Sunset:', sunset);
+
+    // Moon Phase
+    const moonPhase = result.forecast.forecastday[0].astro.moon_phase;
+    console.log('Moon Phase:', moonPhase);
 
     // Icon Image
-    const iconImage = result.current.condition.icon;
-    const textImage = result.current.condition.text;
+    // const iconImage = result.current.condition.icon;
+    // const textImage = result.current.condition.text;
 
     // Writing on html page - connecting the APIs
-    const placeHolder = document.querySelector("#weather-info")
-    placeHolder.innerHTML = `
-        <p>Right now it is ... </p>
-        <p>${cityName}</p>
-        <p>${currentTempCelsius} C
-            <img src="${iconImage}" alt="${textImage}" />
-        ${textImage}</p>
-    `
+    // const placeHolder = document.querySelector("#weather-info")
+    // placeHolder.innerHTML = `
+    //     <p>Right now it is ... </p>
+    //     <p>${cityName}</p>
+    //     <p>${currentTempCelsius} C
+    //         <img src="${iconImage}" alt="${textImage}" />
+    //     ${textImage}</p>
+    // `
+
+
+
+
 
 
 }
 
 // Calling function
-getCurrentWeather();
+WeatheringToday();
 
 
